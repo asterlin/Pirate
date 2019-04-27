0642da2480246d99174d918c49718927f6e045a0
cucheng821015 <wl02856836@gmail.com>
2019-04-28 00:00:00 +0800

    4/27改

--- backstage/backManagerToDb.php
+++ backstage/backManagerToDb.php
@@ -12,37 +12,17 @@
      $manager -> bindValue(':managerAcc', $_REQUEST['managerAcc']);
      $manager ->execute();
 
-    //  $today = getdate();
-    //  date("Y-m-d");  //日期格式化
-    //  $year = $today["year"]; //年
-    //  $month = $today["mon"]; //月
-    //  $day = $today["mday"];  //日
-    //  $hours = $today["hours"]; //時
-    //  $minutes = $today["minutes"]; //分
-    //  $seconds = $today["seconds"]; //秒
-    //  $today_date = $year."-".$month."-".$day." ".$hours.":".$minutes.":".$seconds;
-
-    //  echo $today_date;
-    //  exit();
-
-    //  $tradeTime;
-    //  $tradeTime = new Date();
-    //  $tradeTime = tradeTime.getFullYear() + '-' +
-    //      ('00' + (tradeTime.getMonth() + 1)).slice(-2) + '-' +
-    //      ('00' + tradeTime.getDate()).slice(-2) + ' ' +
-    //      ('00' + tradeTime.getHours()).slice(-2) + ':' +
-    //      ('00' + tradeTime.getMinutes()).slice(-2) + ':' +
-    //      ('00' + tradeTime.getSeconds()).slice(-2);
-  
      if ($manager -> rowCount() != 0) {
-         echo "false";
+        //  echo "false";
      } else {
          $sql = "insert into manager values(:managerAcc, :managerPsw, now(), 1)";
          $manager = $pdo -> prepare($sql);
          $manager -> bindValue(':managerAcc', $_REQUEST['managerAcc']);
          $manager -> bindValue(':managerPsw', $_REQUEST['managerPsw']);
          $manager -> execute();
+         header('location:backManager.php');
      }
+
  } catch (PDOException $e) {
      $errMsg .=  "錯誤原因" . $e->getMessage() . "<br>";
      $errMsg .=  "錯誤行號" . $e->getLine() . "<br>";
---
+++ getMyInfo.php
@@ -0,0 +1,14 @@
+<?php
+$errMsg = "";
+    try{
+        require_once("backstage/php/connectPirate.php");
+        $sql = "select * from member where memId=:memId";
+        $getMyInfo = $pdo->prepare($getMyInfo);
+        $addClickAmt->bindValue(":memId", $_REQUEST["memId"]);
+        $getMyInfo->execute();
+        $getMyInfoRow = $getMyInfo->fetch(PDO::FETCH_ASSOC);
+        echo json_encode($getMyInfoRow);
+    }catch(PDOException $e){
+        echo $e->getMessage();
+        };
+?>
