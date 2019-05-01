b650d254f62b26716d12f4b226ae708ae1237d8e
good9r <covenantee23@gmail.com>
2019-05-01 21:07:22 +0800

    by承

---
+++ change.php
@@ -0,0 +1,30 @@
+<?php
+$salerId = $_REQUEST["salerId"];
+$tradeId = $_REQUEST["tradeId"];
+$treaprice = (int)$_REQUEST["price"];
+$memMoney = (int)$_SESSION["memMoney"];
+$memprice = ($memMoney - $treaprice);
+$salerprice = ($memMoney + $treaprice);
+$memId = 'ww';//$_SESSION['memId']
+try{
+    session_start();
+    $date = date("Y-m-d");
+    $memId = 'sasa'; //$_SESSION["memId"]
+    require_once("connectPirates.php");
+    $sql = "UPDATE traderecord SET buyerId = 'ww',tradeTime = '$date' where tradeId= '$tradeId'";
+    $traderecord = $pdo->exec( $sql );
+
+    $sql = "UPDATE member SET memMoney = 600 where memId= '$memId'";//$memprice
+    $member = $pdo->exec( $sql );
+
+    $sql = "UPDATE member SET memMoney = 500 where memId= '$salerId'";//$salerprice
+    $member = $pdo->exec( $sql );
+    header("Location:black.php");
+  
+  }catch(PDOException $e){
+    
+  }
+
+    
+?>
+
