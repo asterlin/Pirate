b231f7500ac254650539337e38290b3ba50769ed
exoplorer717ed <explorer717ed@gmail.com>
2019-05-01 17:32:04 +0800

    ★★首頁及燈箱以php外連★★

--- js/verification.js
+++ js/verification.js
@@ -118,3 +118,41 @@ function verification(){
 function getrandom(x){
     return Math.floor(Math.random()*x)+1;
 }
+
+//以下是原login的內容
+
+function showLoginLiBo(){
+    loginBox.style.display = 'block';
+}
+
+
+var loginBtns = document.getElementsByClassName('loginHere');
+var loginBox = document.getElementById('loginBox');
+for(i=0;i<loginBtns.length;i++){
+    loginBtns[i].addEventListener('click',showLoginLiBo)
+}
+
+var liBoCloseBtns = document.querySelectorAll('.lightBox .leave');
+for(var i = 0; i<liBoCloseBtns.length;i++){
+    liBoCloseBtns[i].addEventListener('click',function(e){
+        e.target.parentNode.parentNode.style.display="none";
+    })
+}
+
+var liBoBGs = document.querySelectorAll('.lightbox .popbg');
+for(var i=0;i<liBoBGs.length;i++){
+    liBoBGs[i].addEventListener('click',function(e){
+        e.target.parentNode.style.display="none";
+    })
+}
+
+$(function(){
+    var $li = $('ul.tab-title li');
+        $($li. eq(0) .addClass('active').find('a').attr('href')).siblings('.tab-inner').hide();
+    
+        $li.click(function(){
+            $($(this).find('a'). attr ('href')).show().siblings ('.tab-inner').hide();
+            $(this).addClass('active'). siblings ('.active').removeClass('active');
+        });
+    });
+
